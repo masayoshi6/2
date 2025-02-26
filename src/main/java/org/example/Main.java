7de3e22c25849887754ffb2117541e126aa85017
masayoshi6 <okumurakiyoshikou@MacBook-Air.local>
2025-02-26 14:26:25 +0900

    あああ

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
     // IntelliJ IDEA によるその修正案を確認できます。
     System.out.println("Hello and welcome!");
 
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 2; i++) {
       //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
       // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
       System.out.println("i = " + i);
